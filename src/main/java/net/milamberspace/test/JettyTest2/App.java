af8052de6a775bd4baf94475286c7499fc2a9c0d
Milamber <milamber@apache.org>
2018-04-30 09:43:54 +0100

    Initial import

---
+++ src/main/java/net/milamberspace/test/JettyTest2/App.java
@@ -0,0 +1,13 @@
+package net.milamberspace.test.JettyTest2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
